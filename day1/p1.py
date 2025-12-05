69225b12973814d0935a5893ed45d9fe5f0419d0
Alec Tsi <100969572+AlecTsi@users.noreply.github.com>
2025-12-05 05:26:11 +0000

    123

--- day1/p1.py
+++ day1/p1.py
@@ -1,22 +1,30 @@
-start = 50
-path = "/workspaces/2025_AdventOfCode/day1/input1.txt"
-ans = 0
+def main():
+    """
+    dial starts at 50
+    dial goes from 0 - 99
+    Password is the # of times it points at 0 after each rotation
+    """
+    
+    print(get_answer())
 
-with open("/workspaces/2025_AdventOfCode/day1/input1.txt","r") as file:
-    lines = file.readlines()
-    for line in lines:
-        line = line.replace("L", "-")
-        line = line.replace("R","")
-        line = int(line)
-        print(line)
+def get_answer():
+    dial = 50
+    ans = 0
+    path = "/workspaces/2025_AdventOfCode/day1/input1.txt"
 
-        start += line
-        if start % 100 == 0:
+    with open(path) as f:
+        turns = [int(line.strip().replace("L", "-").replace("R","")) for line in f]
+
+    for turn in turns:
+        dial = (dial + turn) % 100
+        
+        if dial == 0:
             ans += 1
             
-print(ans)
-        
-        
+    return ans
+    
+if __name__ == "__main__":
+    main()    
         
         
 
--- day1/p2.py
+++ day1/p2.py
@@ -1,20 +1,30 @@
-path = "/workspaces/2025_AdventOfCode/day1/input1.txt"
-start = 50
-ans = 0
-a2 = 0
-with open(path, "r") as file:
-    for line in file:
-        clean_line = line.strip()
-        clean_line = clean_line.replace("L", "-").replace("R", "")
-        value = int(clean_line)
-        start += value
-        a,b = divmod(start, 100)
-        # Check condition
-        if b % 100 == 0:
+def main():
+    """
+    dial starts at 50
+    dial goes from 0 - 99
+    Password is the # of times it points at 0 after each rotation
+    """
+    
+    print(get_answer())
+
+def get_answer():
+    dial = 50
+    ans = 0
+    path = "/workspaces/2025_AdventOfCode/day1/input1.txt"
+
+    with open(path) as f:
+        turns = [int(line.strip().replace("L", "-").replace("R","")) for line in f]
+
+    for turn in turns:
+        dial = (dial + turn) % 100
+        
+        if dial == 0:
             ans += 1
-            ans += abs(a-a2) - 1
-            a2 = a
-        else:
-            ans += abs(a-a2)
-            a2 = a
-print(f"Final Answer: {ans}")
+            
+    return ans
+    
+if __name__ == "__main__":
+    main()    
+        
+        
+
